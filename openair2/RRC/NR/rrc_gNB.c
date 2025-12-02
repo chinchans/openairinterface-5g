c2e2561fbe03d6e48b11fb680ccf5230059d8436
chinchans <vangalachandu77@gmail.com>
2025-12-02 09:26:44 +0000

    Fix NGAP protocol issue
    Error Message: "Segmentation Fault in rrc_gNB_send_NGAP_NAS_FIRST_REQ at /home/tcs/jayasvi/cu/openairinterface5g/openair2/RRC/NR/rrc_gNB_NGAP.c:173"
    
    Applied 3 code patches and 0 config patches.
    
    Selected Patch Details:
    - Code: rrc_gNB_process_RRCSetupComplete (rrc_gNB.c) (rrc_gNB.c)
      Description: Remove the rrcSetupComplete=NULL statement that was setting parameter to NULL before use
    - Code: rrc_gNB_process_RRCSetupComplete (rrc_gNB.c) (rrc_gNB.c)
      Description: Remove the rrcSetupComplete=NULL statement that was setting parameter to NULL before use
    - Code: rrc_gNB_send_NGAP_NAS_FIRST_REQ (rrc_gNB_NGAP.c) (rrc_gNB_NGAP.c)
      Description: Add NULL check for dedicatedNAS_Message.buf before using it

--- openair2/RRC/NR/rrc_gNB.c
+++ openair2/RRC/NR/rrc_gNB.c
@@ -510,6 +510,12 @@ static void rrc_gNB_process_RRCSetupComplete(gNB_RRC_INST *rrc, gNB_RRC_UE_t *UE
 //-----------------------------------------------------------------------------
 {
   UE->Srb[1].Active = 1;
+      if (!rrcSetupComplete) {
+    LOG_E(RRC, "Invalid param\n");
+    return;
+  }
+  rrc_gNB_send_NGAP_NAS_FIRST_REQ(rrc, UE, rrcSetupComplete);
+      // REMOVED: rrcSetupComplete=NULL;
   UE->Srb[2].Active = 0;
   
   rrcSetupComplete=NULL;
